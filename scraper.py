64606fbf0bd7e76112300838d74534596fcb0a79
s-013-19 <dk1232004@gmail.com>
2022-04-05 13:02:20 +0200

    2022-04-05 DK

--- scraper.py
+++ scraper.py
@@ -1,21 +1,28 @@
-from bs4 import BeautifulSoup
 import requests
-
+from bs4 import BeautifulSoup
 
 url = "https://www.ceneo.pl/45863470#tab=reviews"
 
-r = requests.get(url)
+response = requests.get(url)
 
-if r.status_code == 200:
-    print('[INFO]: Roger.')
+page_dom = BeautifulSoup(response.text, "html.parser")
 
-    soup = BeautifulSoup(r.text, 'html.parser')
-    # print(soup.prettify())
-    opinions = soup.select("div.js_product-review")
-    opinion=opinions.pop()
-    print(type(opinion))
-    
+opinions = page_dom.select("div.js_product-review")
+opinion = opinions.pop(3)
 
+opinion_id = opinion["data-entry-id"]
+author = opinion.select_one("span.user-post__author-name").text.strip()
+rcmd = opinion.select_one("span.user-post__author-recomendation > em").text.strip()
+score = opinion.select_one("span.user-post__score-count").text.strip()
+content = opinion.select_one("div.user-post__text").text.strip()
+posted_on = opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"]
+bought_on = opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"]
+useful_for = opinion.select_one("button.vote-yes > span").text.strip()
+useless_for = opinion.select_one("button.vote-no > span").text.strip()
+pros = opinion.select("div.review-feature__title--positives ~ div.review-feature__item")
+pros = [item.text.strip() for item in pros]
+cons = opinion.select("div.review-feature__title--negatives ~ div.review-feature__item")
+cons = [item.text.strip() for item in cons]
 
-else:
-    print(f"[INFO]: Something went wrong...\nStatus code: {r.status_code}")
+print(type(cons))
+print(cons)
